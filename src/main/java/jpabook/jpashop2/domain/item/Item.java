2e157fa36bc82c5787ade64e0bde6fdc9d07c7ed
주현태(Hyeontae Ju)/기프티콘개발팀/11ST <juhyeontae@11sk.com>
2023-04-23 16:48:59 +0900

    상품 서비스 개발
    상품 서비스 개발

--- src/main/java/jpabook/jpashop2/domain/item/Item.java
+++ src/main/java/jpabook/jpashop2/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop2.domain.item;
 
 import jpabook.jpashop2.domain.Category;
 import jpabook.jpashop2.domain.OrderItem;
+import jpabook.jpashop2.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,7 +20,6 @@ import java.util.List;
 
 @Entity
 @Getter
-@Setter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 public abstract class Item {
@@ -40,4 +40,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // == 비즈니스 로직 == //
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감속
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
