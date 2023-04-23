64f1ffd0fb8b9746d16566af925a25fda7721471
주현태(Hyeontae Ju)/기프티콘개발팀/11ST <juhyeontae@11sk.com>
2023-04-23 19:55:59 +0900

    주문 기능개발
    JPQL 및 Criteria에 대해 알아보았는데,
    동적 쿼리를 위해 query dsl의 필요성을 보여주는듯.

--- src/main/java/jpabook/jpashop2/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop2/repository/OrderRepository.java
@@ -3,8 +3,18 @@ package jpabook.jpashop2.repository;
 import jpabook.jpashop2.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,7 +30,45 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+
+        String jpql = "select o from Order o join o.member m" +
+                " where o.status = :status" +
+                " and m.name like :name";
+
+        return em.createQuery(jpql, Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1_000)
+                .getResultList();
+    }
+
+    /**
+     * JPA Criteira
+     * 유지보수성이 zero에 가깝다.r
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        // 주문상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        // 회원이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name = cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1_000);
+        return query.getResultList();
+    }
 }
---
+++ src/main/java/jpabook/jpashop2/repository/OrderSearch.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop2.repository;
+
+import jpabook.jpashop2.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
+}
